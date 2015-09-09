605080e9f590bcd24e26cfb69d3c02f21d449c78
Kyle Kelley <rgbkrk@gmail.com>
2015-09-08 23:01:37 -0500

    progress bar!

--- gen.sh
+++ gen.sh
@@ -1,19 +1,64 @@
 #!/usr/bin/env bash
+# Generates the go code for ALL the gitignores,
+# via github.com/github/gitignore
+
+preparebar() {
+# $1 - bar length
+# $2 - bar char
+    barlen=$1
+    barspaces=$(printf "%*s" "$1")
+    barchars=$(printf "%*s" "$1" | tr ' ' "$2")
+}
+
+progressbar() {
+# $1 - number (-1 for clearing the bar)
+# $2 - max number
+    if [ $1 -eq -1 ]; then
+        printf "\r  $barspaces\r"
+    else
+        barch=$(($1*barlen/$2))
+        barsp=$((barlen-barch))
+        printf "\r[%.${barch}s%.${barsp}s]\r" "$barchars" "$barspaces"
+    fi
+}
+
+clonedLocation="ghignore"
+
+if [ ! -d $clonedLocation ]
+then
+  git clone https://github.com/github/gitignore $clonedLocation
+else
+  pushd . >> /dev/null
+  cd $clonedLocation
+  git pull >> /dev/null
+  popd >> /dev/null
+fi
+
 mkdir -p langs
+langGo="langs/langs.go"
 
-langGo="langs.go"
-
-echo "package gitignores
+echo "package langs
 
 // Gitignores is the generated map of language -> gitignore
 var Gitignores = map[string]string{" > $langGo
 
-for gitignore in gitignore-cloned/*.gitignore; do
+gitignores=($clonedLocation/*.gitignore)
+count=${#gitignores[@]}
+
+preparebar $count "#"
+let "start=0"
+
+for gitignore in $clonedLocation/*.gitignore; do
   lang=`basename $gitignore .gitignore`
 
   printf "  \"%s\": \`" $lang >> $langGo
   cat $gitignore | sed 's/`/` + "`" + `/g'>> $langGo
   printf "\`,\n" >> $langGo
+  let "start++"
+  progressbar $start $count
 done
 
 echo "}" >> $langGo
+
+echo ""
+echo "Generated gitignore for go!"
